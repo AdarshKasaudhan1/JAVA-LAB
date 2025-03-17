9824e99990e668bf3be60c0037a56f761ad42d27
AdarshKasaudhan1 <158551590+AdarshKasaudhan1@users.noreply.github.com>
2025-03-17 10:47:40 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,16 @@
-//file handling in java: Writing a file
-import java.io.FileWriter;
-import java.io.IOException; // exception means error in files
-public class Main{
-    public static void main(String[] args) {
-        try{
-            FileWriter java = new FileWriter("java.txt"); // creating and writing a file
-            java.write("Java is a High Level Programming Language");
-            java.close();
-            System.out.println("Java File Created and Writing is Finish");
-        }catch(IOException e){
-            System.out.println("File is Not Created : Error");
-            e.printStackTrace();
-        }
+//thread in java
+package sample;
+
+class Child extends Thread{//child class
+    public void run(){
+        System.out.println("this is Infinitive");
+    }
+}
+public class Main {
+    public static void main(String[]args){
+        Child ak =new Child();
+
+        ak.run();
 
     }
 }
